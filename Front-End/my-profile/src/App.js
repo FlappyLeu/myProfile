975cba47e3bdf02138c4b7a33ce4e297e78cba57
mstars_lab_04 <mstars_lab_04@mstars-lab-04s-iMac.local>
2022-07-18 09:56:53 +0800

    dsa

--- Front-End/my-profile/src/App.js
+++ Front-End/my-profile/src/App.js
@@ -1,42 +1,7 @@
-import { infoService } from "./components/infetch/getInfo";
-import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
-  const [name, setName] = useState();
-  const [age, setAge] = useState();
-  const [major, setMajor] = useState();
-  const [profile, setProfile] = useState();
-  const [desc, setDesc] = useState();
-  useEffect(() => {
-    infoService
-      .getName()
-      .then((res) => res.json())
-      .then((data) => setName(data));
-    infoService
-      .getAge()
-      .then((res) => res.json())
-      .then((age) => setAge(age));
-    infoService
-      .getMajor()
-      .then((res) => res.json())
-      .then((major) => setMajor(major));
-    infoService
-      .getProfile()
-      .then((res) => res.json())
-      .then((data) => setProfile(data));
-    infoService
-      .getDescription()
-      .then((res) => res.json())
-      .then((data) => setDesc(data));
-  }, []);
-  console.log(name);
-  return (
-    <div className="age">
-      <h1>{name && name.name}</h1>
-      <div>{age && age.age}</div>
-      <div>{major && major.major}</div>
-      <div>{desc && desc.description}</div>
-    </div>
-  );
+  return <>headers</>;
 }
 
 export default App;
